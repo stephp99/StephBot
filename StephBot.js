aba302ff4c9dafbb5264dbe611858872186c2690
stephp99 <45298288+stephp99@users.noreply.github.com>
2018-11-23 22:32:53 -0500

    Update StephBot.js

--- StephBot.js
+++ StephBot.js
@@ -104,7 +104,6 @@ client.on ("message", (message) => {
 	
 	mention = message.mentions.users.first();
 	msg = message.content.toLowerCase();
-	//for (x=0; x < profanitiez.length; x++) {
 		
   	var words = message.content.toLowerCase().trim().match(/\w+|\s+|[^\s\w]+/g);
   	var containsBadWord = words.some(word => {
@@ -114,18 +113,6 @@ client.on ("message", (message) => {
     		message.delete(1);
     		message.channel.send("LANGUAGE <:Soap:515638223704162346>");
   	}
-
-		//if (msg.includes(profanitiez[x])) {
-		//	message.channel.send("LANGUAGE <:Soap:515638223704162346>");
-		//	message.delete();
-		//	return;
-		
-		//if (message.content.toUpperCase() == profanities[x].toUpperCase()) {
-		//	message.channel.send("LANGUAGE <:Soap:515638223704162346>");
-		//	message.delete();
-		//	return;
-		//}
-	//}
 	
 	if(message.content.includes("hug")) {
 		if (message.isMentioned(client.user)) {
@@ -229,6 +216,46 @@ client.on ("message", (message) => {
 			});
 		}
 	}
+	
+	if(message.content.startsWith (prefix + 'duel') {
+    	//checks if the username to fight is in the message
+    	let author1 = message.author.username;
+    	let user = message.mentions.users.first();
+    	if(!user) return message.reply("you did not specify who you would like to fight!");
+
+    	//checks if the users is trying to fight themselves
+    	if(user.id == message.author.id) return message.reply('you cannot fight yourself!');
+
+    	//checks if the user is trying to fight the bot
+    	if(user.bot ==  true)
+        return message.reply('you cannot fight a bot!');
+
+    	//saves the two user ids to variables
+    	var fighter1 = message.author.id;
+    	var fighter2 = user.id;
+
+    	//announces challenge and awaits response
+    	var challenged = user.toString();
+    	message.channel.send(`${challenged}, ${author1} has challenged you to a duel. Do you accept the challenge, yes or no?`)
+        	.then(() => {
+            	message.channel.awaitMessages(response => response.content == 'yes' && response.author.id == fighter2 || response.content == 'no' && response.author.id == fighter2, {
+                	max: 1,
+                	time: 60000,
+                	errors: ['time'],
+            	})
+            	.then((collected) => {
+                	if (collected.first().content == 'yes') {
+                    		message.channel.send(`${challenged} has accepted the challenge!`);
+               		}
+                	else if(collected.first().content == 'no') {
+                    		message.channel.send(`nope`);
+                	}
+            	})
+            	.catch(() => {
+                	message.channel.send(`No response. Fight has been cancelled.`);
+            	});
+        });       
+}
 });
 
 client.login (token);
